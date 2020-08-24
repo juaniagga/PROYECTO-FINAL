ff6e2025a4424e9c80499bec134d6bce9d4463f8
pilar <pilarfernandezmutti@gmail.com>
2020-08-24 12:48:38 -0300

    Jquery hasta 247 inclusive

--- js/main.js
+++ js/main.js
@@ -66,3 +66,39 @@
     }); // DOM CONTENT LOADED
 })();
 
+$(function() {
+
+    //Programa de charlas/stands
+    
+    $('.programa-evento .info-curso:first').show();  //agarra "charlas"
+    $('.menu-programa a:first').addClass('activo');  //que el primero "charlas" se ponga en activo
+    $('.menu-programa a').on('click',function(){
+        $('.menu-programa a').removeClass('activo');   //para que solo se muestren activos los que estan siendo cliqueados en el momento y no todos
+        $(this).addClass('activo');
+        $('.ocultar').hide();
+        var enlace = $(this).attr('href');
+        $(enlace).fadeIn(1000);
+        return false;
+    });
+
+
+    //Animaciones para los numeros
+    $('.resumen-evento li:nth-child(1) p').animateNumber({ number: 2000 }, 1200);
+    $('.resumen-evento li:nth-child(2) p').animateNumber({ number: 150 }, 1200);
+    $('.resumen-evento li:nth-child(3) p').animateNumber({ number: 3 }, 500);
+    $('.resumen-evento li:nth-child(4) p').animateNumber({ number: 80 }, 1200);
+
+    //Cuenta regresiva
+
+    $('.cuenta-regresiva').countdown('2020/12/10 09:00:00', function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+
+
+
+
+
+});
